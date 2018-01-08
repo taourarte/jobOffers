c7df2c836254382adab0d3b5d674845bf64e8e77
AITAOURARTE <aissa.taourarte@gmail.com>
2018-01-08 10:48:26 +0000

    my seconde commit

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,7 @@
 const noteRoutes = require('./note_routes');
 module.exports = function(app, db) {
+    console.log("before route..");
   noteRoutes(app, db);
+  console.log("after route");
   // Other route groups could go here, in the future
 };
